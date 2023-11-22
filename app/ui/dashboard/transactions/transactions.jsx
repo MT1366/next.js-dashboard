828a1061ad5061d66cf9759b68c76d97db0a2d9c
milad <miladtavassoli13@gmail.com>
2023-11-22 23:58:19 +0330

    table, chart and rightbar were implemented

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,6 +1,84 @@
 import React from "react";
 import styles from "./transactions.module.css";
+import Image from "next/image";
 
 export default function Transactions() {
-  return <div className={styles.conitaner}>transactions</div>;
+  return (
+    <div className={styles.conitaner}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/IMG_7570.JPG"
+                  alt=""
+                  width={40}
+                  height={20}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$300</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/IMG_7570.JPG"
+                  alt=""
+                  width={40}
+                  height={20}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$300</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/IMG_7570.JPG"
+                  alt=""
+                  width={40}
+                  height={20}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Canceled
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$300</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
